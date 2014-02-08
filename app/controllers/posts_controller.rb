77f419eb3ad5814cd57538c2fa146549b0e8af6d
Louisa Barrett <louisabarrett@gmail.com>
2014-02-08 10:49:09 -0700

    add edit to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,18 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  # private
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-  #   def post_params
-  #     params.require(:post).permit(:title, :text)
-  #   end
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
 end
